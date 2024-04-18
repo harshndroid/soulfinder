98a2358b92bd79440216c33c5714733f1bfba0c5
harsh-chalo <52203103+harshndroid@users.noreply.github.com>
2024-04-18 23:38:19 +0530

    removed log

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import AppRoutes from './routes';
 
 function App() {
-  console.log('===logs');
   window.Buffer = window.Buffer || require('buffer').Buffer;
   return <AppRoutes />;
 }
